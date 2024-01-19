be0754a481846c06b862060cc9f9d4e997b71de1
Dibakar Mandal <dibakar11081990@gmail.com>
2024-01-19 23:07:36 +0530

    glue

---
+++ aws_boto_anjali_sharma/src/glue.py
@@ -0,0 +1,362 @@
+import boto3
+import logging
+from botocore.exceptions import ClientError
+
+class GlueWrapper:
+
+    """Encapsulates AWS Glue actions."""
+
+    def __init__(self, glue_client):
+        """
+        :param glue_client: A Boto3 Glue client.
+        """
+        self.glue_client = glue_client
+
+        self.logger = logging.getLogger(__name__)
+        logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+
+    def get_crawler(self, name):
+        """
+        Gets information about a crawler.
+
+        :param name: The name of the crawler to look up.
+        :return: Data about the crawler.
+        """
+        crawler = None
+        try:
+            response = self.glue_client.get_crawler(Name=name)
+            crawler = response["Crawler"]
+        except ClientError as err:
+            if err.response["Error"]["Code"] == "EntityNotFoundException":
+                self.logger.info("Crawler %s doesn't exist.", name)
+            else:
+                self.logger.error(
+                    "Couldn't get crawler %s. Here's why: %s: %s",
+                    name,
+                    err.response["Error"]["Code"],
+                    err.response["Error"]["Message"],
+                )
+                raise
+        return crawler
+
+
+    def create_crawler(self, name, role_arn, db_name, db_prefix, s3_target):
+        """
+        Creates a crawler that can crawl the specified target and populate a 
+        database in our AWS Glue Data Catalog with metadata that describes the data
+        in the target.
+
+        :param name: The name of the crawler.
+        :param role_arn: The Amazon Resource Name (ARN) of an AWS Identity and Access
+                         Management (IAM) role that grants permission to let AWS Glue
+                         access the resources it needs.
+        :param db_name: The name to give the database that is created by the crawler.
+        :param db_prefix: The prefix to give any database tables that are created by
+                          the crawler.
+        :param s3_target: The URL to an S3 bucket that contains data that is
+                          the target of the crawler.
+        """
+        try:
+            self.glue_client.create_crawler(
+                Name=name,
+                Role=role_arn,
+                DatabaseName=db_name,
+                TablePrefix=db_prefix,
+                Targets={"S3Targets": [{"Path": s3_target}]},
+            )
+        except ClientError as err:
+            self.logger.error(
+                "Couldn't create crawler. Here's why: %s: %s",
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
+
+
+    def start_crawler(self, name):
+        """
+        Starts a crawler. The crawler crawls its configured target and creates
+        metadata that describes the data it finds in the target data source.
+
+        :param name: The name of the crawler to start.
+        """
+        try:
+            self.glue_client.start_crawler(Name=name)
+        except ClientError as err:
+            self.logger.error(
+                "Couldn't start crawler %s. Here's why: %s: %s",
+                name,
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
+
+
+    def get_database(self, name):
+        """
+        Gets information about a database in your Data Catalog.
+
+        :param name: The name of the database to look up.
+        :return: Information about the database.
+        """
+        try:
+            response = self.glue_client.get_database(Name=name)
+        except ClientError as err:
+            self.logger.error(
+                "Couldn't get database %s. Here's why: %s: %s",
+                name,
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
+        else:
+            return response["Database"]
+
+
+    def get_tables(self, db_name):
+        """
+        Gets a list of tables in a Data Catalog database.
+
+        :param db_name: The name of the database to query.
+        :return: The list of tables in the database.
+        """
+        try:
+            response = self.glue_client.get_tables(DatabaseName=db_name)
+        except ClientError as err:
+            self.logger.error(
+                "Couldn't get tables %s. Here's why: %s: %s",
+                db_name,
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
+        else:
+            return response["TableList"]
+
+
+    def create_job(self, name, description, role_arn, script_location):
+        """
+        Creates a job definition for an extract, transform, and load (ETL) job that can
+        be run by AWS Glue.
+
+        :param name: The name of the job definition.
+        :param description: The description of the job definition.
+        :param role_arn: The ARN of an IAM role that grants AWS Glue the permissions
+                         it requires to run the job.
+        :param script_location: The Amazon S3 URL of a Python ETL script that is run as
+                                part of the job. The script defines how the data is
+                                transformed.
+        """
+        try:
+            self.glue_client.create_job(
+                Name=name,
+                Description=description,
+                Role=role_arn,
+                Command={
+                    "Name": "glueetl",
+                    "ScriptLocation": script_location,
+                    "PythonVersion": "3",
+                },
+                GlueVersion="3.0",
+            )
+        except ClientError as err:
+            self.logger.error(
+                "Couldn't create job %s. Here's why: %s: %s",
+                name,
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
+
+
+    def start_job_run(self, name, input_database, input_table, output_bucket_name):
+        """
+        Starts a job run. A job run extracts data from the source, transforms it,
+        and loads it to the output bucket.
+
+        :param name: The name of the job definition.
+        :param input_database: The name of the metadata database that contains tables
+                               that describe the source data. This is typically created
+                               by a crawler.
+        :param input_table: The name of the table in the metadata database that
+                            describes the source data.
+        :param output_bucket_name: The S3 bucket where the output is written.
+        :return: The ID of the job run.
+        """
+        try:
+            # The custom Arguments that are passed to this function are used by the
+            # Python ETL script to determine the location of input and output data.
+            response = self.glue_client.start_job_run(
+                JobName=name,
+                Arguments={
+                    "--input_database": input_database,
+                    "--input_table": input_table,
+                    "--output_bucket_url": f"s3://{output_bucket_name}/",
+                },
+            )
+        except ClientError as err:
+            self.logger.error(
+                "Couldn't start job run %s. Here's why: %s: %s",
+                name,
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
+        else:
+            return response["JobRunId"]
+
+
+    def list_jobs(self):
+        """
+        Lists the names of job definitions in your account.
+
+        :return: The list of job definition names.
+        """
+        try:
+            response = self.glue_client.list_jobs()
+        except ClientError as err:
+            self.logger.error(
+                "Couldn't list jobs. Here's why: %s: %s",
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
+        else:
+            return response["JobNames"]
+
+
+    def get_job_runs(self, job_name):
+        """
+        Gets information about runs that have been performed for a specific job
+        definition.
+
+        :param job_name: The name of the job definition to look up.
+        :return: The list of job runs.
+        """
+        try:
+            response = self.glue_client.get_job_runs(JobName=job_name)
+        except ClientError as err:
+            self.logger.error(
+                "Couldn't get job runs for %s. Here's why: %s: %s",
+                job_name,
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
+        else:
+            return response["JobRuns"]
+
+
+    def get_job_run(self, name, run_id):
+        """
+        Gets information about a single job run.
+
+        :param name: The name of the job definition for the run.
+        :param run_id: The ID of the run.
+        :return: Information about the run.
+        """
+        try:
+            response = self.glue_client.get_job_run(JobName=name, RunId=run_id)
+        except ClientError as err:
+            self.logger.error(
+                "Couldn't get job run %s/%s. Here's why: %s: %s",
+                name,
+                run_id,
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
+        else:
+            return response["JobRun"]
+
+
+    def delete_job(self, job_name):
+        """
+        Deletes a job definition. This also deletes data about all runs that are
+        associated with this job definition.
+
+        :param job_name: The name of the job definition to delete.
+        """
+        try:
+            self.glue_client.delete_job(JobName=job_name)
+        except ClientError as err:
+            self.logger.error(
+                "Couldn't delete job %s. Here's why: %s: %s",
+                job_name,
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
+
+
+    def delete_table(self, db_name, table_name):
+        """
+        Deletes a table from a metadata database.
+
+        :param db_name: The name of the database that contains the table.
+        :param table_name: The name of the table to delete.
+        """
+        try:
+            self.glue_client.delete_table(DatabaseName=db_name, Name=table_name)
+        except ClientError as err:
+            self.logger.error(
+                "Couldn't delete table %s. Here's why: %s: %s",
+                table_name,
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
+
+
+    def delete_database(self, name):
+        """
+        Deletes a metadata database from your Data Catalog.
+
+        :param name: The name of the database to delete.
+        """
+        try:
+            self.glue_client.delete_database(Name=name)
+        except ClientError as err:
+            self.logger.error(
+                "Couldn't delete database %s. Here's why: %s: %s",
+                name,
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
+
+
+    def delete_crawler(self, name):
+        """
+        Deletes a crawler.
+
+        :param name: The name of the crawler to delete.
+        """
+        try:
+            self.glue_client.delete_crawler(Name=name)
+        except ClientError as err:
+            self.logger.error(
+                "Couldn't delete crawler %s. Here's why: %s: %s",
+                name,
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
+
+
+
+
+if __name__ == "__main__":
+
+    reg_name = "ap-south-1"
+    aws_management_console = boto3.session.Session(profile_name='boto3_user')
+    glue_client = aws_management_console.client('glue', region_name=reg_name)
+    gl = GlueWrapper(glue_client)
+
+    # gl.create_crawler(name='my_1st_crawler', role_arn='arn:aws:iam::245491356924:role/Glue_role', db_name='test_db', db_prefix='glow', s3_target='s3://testbucket20k/input/')
+    # gl.get_crawler(name='my_1st_crawler')
+    # gl.get_database(name='test_db')
+    # gl.get_tables(db_name='test_db')
+
+
